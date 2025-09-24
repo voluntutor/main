0f18f58b7fd460975a0bae96ac98841e60a67d51
Aarush <78714918+AarushAce@users.noreply.github.com>
2025-09-24 09:27:46 -0400

    made some changes to register
    html: not so much changes
    js: not able to figure that out yet

--- register/register.js
+++ register/register.js
@@ -1,29 +1,45 @@
 // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCiiAfvZ25ChvpCVCbMj46dsCrZBYMGBpM",
-    authDomain: "logintrial001.firebaseapp.com",
-    projectId: "logintrial001",
-    storageBucket: "logintrial001.firebasestorage.app",
-    messagingSenderId: "419688887007",
-    appId: "1:419688887007:web:013af1286ba6e7aff42f6a"
-  };
+import { getAuth, createUserWithEmailAndPassword } from "https://www.getstatic.com/firebase/12.3.0/firebase-auth.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiiAfvZ25ChvpCVCbMj46dsCrZBYMGBpM",
+  authDomain: "logintrial001.firebaseapp.com",
+  projectId: "logintrial001",
+  storageBucket: "logintrial001.firebasestorage.app",
+  messagingSenderId: "419688887007",
+  appId: "1:419688887007:web:013af1286ba6e7aff42f6a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-  //Inputs
+//Inputs
+
+
+//submit button
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-
-  //submit button
-  const submit = document.getElementById('submit');
-  submit.addEventListener("click", function(event){
-    event.preventDefault();
-    alert(5);
-  })
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      alert("You are signed up ")
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("error")
+      // ..
+    });
+})
